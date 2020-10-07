216fa6086190b97d7d9183092517f7668360834d
m-gallego <71590830+m-gallego@users.noreply.github.com>
2020-10-07 22:22:44 +0200

    Update Rfid.rb

--- Rfid.rb
+++ Rfid.rb
@@ -1,14 +1,19 @@
 require 'mfrc522'
 
 class Rfid
-	def read_uid
-		r = MFRC522.new
-		r.picc_request(MFRC522::PICC_REQA)
-		begin
-   		uid, sak = r.picc_select
-   		puts "uid: #{uid}"
-		end		
-	end
+    def read_uid
+        loop do
+        r = MFRC522.new
+        r.picc_request(MFRC522::PICC_REQA)
+        begin
+           uid, sak = r.picc_select
+           puts "%02x%02x%02x%02x" % uid
+        break uid if uid
+        rescue
+        redo
+        end        
+    end
+end
 end
 
 if __FILE__==$0
@@ -17,3 +22,4 @@ rf = Rfid.new
 uid = rf.read_uid
 
 end
+
